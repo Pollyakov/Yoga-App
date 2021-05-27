4f9900bcf8e5194d828bb3bbca0d9bf9c8e20456
Ksenia Gountsova <ksusha.net@gmail.com>
2021-05-27 10:59:04 +0300

    landing page and Navbar design

--- yogaapp/src/Pages/LandingPage.js
+++ yogaapp/src/Pages/LandingPage.js
@@ -1,16 +1,32 @@
 
 import Sequences from "./Sequences";
 import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+// import styled, { css } from 'styled-components';
+
 
 const LandingPage = () => {
-  
+  const useStyles = makeStyles(theme => ({
+    color: {
+    backgroundColor: "#f3ecdb", //antiquewhite
+    }
+  })
+  )
+    const classes = useStyles();
+
+
+
   return (
-    <Container maxWidth="md">
+    
+    <Container 
+    maxWidth="md"
+    className={classes.color}>
 
     <h1>Your Sequences</h1>
       <br/>
       <Sequences/>
       </Container> 
+    
   );
 };
 
